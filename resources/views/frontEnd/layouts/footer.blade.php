1265cbcbe5ab9be2c5f31167ca687f009f79f132
Neeraj Amoli <neeraj.amoli200@gmail.com>
2021-05-06 15:08:16 +0530

    footer updated

--- resources/views/frontEnd/layouts/footer.blade.php
+++ resources/views/frontEnd/layouts/footer.blade.php
@@ -148,7 +148,7 @@
     <div class="footer-bottom">
         <div class="container">
             <div class="row">
-                <p class="pull-left">Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
+                <p class="pull-left">Copyright © 2021 E-SHOPPER Inc. All rights reserved.</p>
                 <p class="pull-right">Designed by <span><a target="_blank" href="#">WeShare</a></span></p>
             </div>
         </div>
